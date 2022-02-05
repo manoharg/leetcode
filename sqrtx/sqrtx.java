cca603b098ffbc05e8732292e722d9c8d6e8445b
Manohar Golla <golamanohar.gm@gmail.com>
2022-02-05 11:50:12 +0530

    Time: 3 ms, Memory: 41.6 MB - LeetHub

--- sqrtx/sqrtx.java
+++ sqrtx/sqrtx.java
@@ -5,6 +5,14 @@ class Solution {
         int l =1;
         int r =x;
         int ans = 1;
+        // We are looking for last T
+        /*
+        1 2 3 4 5 6 7 8
+        If m*m<=x, T
+        For x=8
+        T T F F F F F F 
+        */
+            
         while(l<=r){
             int  m= l+ (r-l)/2;
             if((long)m*m>x){
