60bf69b4aae7128807f7cef952f5e539a5194d8a
Manohar Golla <golamanohar.gm@gmail.com>
2022-02-07 08:58:22 +0530

    Time: 1 ms (52.45%), Space: 42 MB (14.45%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        // [3,1] 1
+     int l =0;
+        int r = nums.length-1;
+        while(l<=r){
+            int m = l+(r-l)/2;
+            if(nums[m]==target){
+                return m;
+            }
+            
+            //left is sorted. = for only one element
+            if(nums[l]<=nums[m]){
+                // element exists in left
+                if(target<nums[m] && target>=nums[l])
+                r=m-1;
+                else
+                   l= m+1; 
+            }else{
+                // exists in right part
+                if(target>nums[m]&&target<=nums[r])
+                l=m+1;
+                else
+                r=m-1;
+            }
+        }
+        
+        return -1;
+    }
+}
