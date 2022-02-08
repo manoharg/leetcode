53a4ae10e6475e556d9c618d3e04b9fd78c1c025
Manohar Golla <golamanohar.gm@gmail.com>
2022-02-08 07:27:58 +0530

    Time: 6 ms (66.08%), Space: 61.5 MB (23.82%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,7 +1,39 @@
 class Solution {
+
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        
-    int lo = 0;
+        //-------- Main idea behind the binary search algorithm ----------//
+        // 1) res will be a consecutive subarray of k size
+        // 2) say if we need to pick 4 elems, now we r looking at 5 elem n1, n2, n3, n4, n5
+        //    we need to compare two ends: diff(x, n1) and diff(x, n5),
+        //    the number with bigger diff on the end will be eleminated
+        //----------------------------------------------------------------//
+        // lo and hi: range of all possible start of subarray with size k + 1, so we could compare both ends
+        int lo = 0, hi = arr.length - k - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            // for subarray starting at mid with size k+1, we compare element of two ends to eliminate the loser
+            if (x - arr[mid] > arr[mid + k] - x) {
+                // arr[mid] is the one further away from x, eliminate range[lo, mid]
+                lo = mid + 1;
+            } else {
+                // arr[mid+k] is the one further away, all [mid, hi] will have simiar situation, elimiate them
+                hi = mid - 1;
+            }
+        }
+        // return subarray
+        List<Integer> res = new ArrayList(k);
+        for (int i = 0; i < k; i++) {
+            res.add(arr[lo + i]);
+        }
+        // return Arrays.stream(A, left, left + k).boxed().collect(Collectors.toList());
+        return res;
+    }
+}
+/*
+O(n) using two pointer
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int lo = 0;
 		int hi = arr.length - 1;
 		while (hi - lo >= k) {
 			if (Math.abs(arr[lo] - x) > Math.abs(arr[hi] - x)) {
@@ -17,5 +49,4 @@ class Solution {
 		return result;
     }
 }
-// 539272669979
-// 517414505517
+*/
