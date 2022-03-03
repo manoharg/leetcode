eab6b2b4ab2cba1b568b580fb0bfaaf4da6359d4
Manohar Golla <golamanohar.gm@gmail.com>
2022-03-03 11:20:27 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (6.96%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -1,27 +1,17 @@
 class Solution {
+
     public int numberOfArithmeticSlices(int[] nums) {
-        if(nums==null || nums.length<3)
-            return 0;
-        int l=nums.length;
-        int count =0;
-        for(int i=0;i<l;i++){
-            int diff=0;
-            for(int j=i;j<l;j++){
-                if(j==i)
-                    continue;
-                if(j==i+1){
-                    diff = nums[j]-nums[j-1];
-                    continue;
-                }
-               
-                if(nums[j]-nums[j-1]==diff){
-                   // System.out.println("ele "+ nums[j]+"diff "+ diff+"j "+j+"i "+i);
-                    if(j-i+1>=3)
-                    count++;
-                }
-                else
-                    break;
-            }
+        if (nums == null || nums.length < 3) return 0;
+        int l = nums.length;
+        int[] dp= new int[l];
+        if(nums[1]-nums[0]==nums[2]-nums[1])
+            dp[2]=1;
+        int count=dp[2];
+        for (int i = 3; i < l; i++) {
+            if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]){
+                dp[i]= dp[i-1]+1;
+                count+=dp[i];
+            }       
         }
         return count;
     }
