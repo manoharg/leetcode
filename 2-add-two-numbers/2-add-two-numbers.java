02d1436506b2c81757253aedc8db29ae429a85ee
Manohar Golla <golamanohar.gm@gmail.com>
2022-03-13 18:25:28 +0530

    Time: 3 ms (73.36%), Space: 47.8 MB (38.35%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -3,78 +3,38 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    public ListNode reverse(ListNode head)
-    {
-        ListNode prev=null;
-        while(head!=null)
-        {
-            ListNode next= head.next;
-            head.next= prev;
-            prev=head;
-            head=next;
-        }
-        return prev;
-    }
-    public void print(ListNode l1)
-    {
-        ListNode temp=l1;
-        while(temp!=null)
-        {
-            System.out.print(temp.val+"-->");
-            temp= temp.next;
-        }
-        System.out.println();
-    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
-   //     l1= reverse(l1);
-     //   l2=reverse(l2);
-       // print(l1);
-       // print(l2);
-        ListNode temp= add(l1,l2);
-        //print(temp);
-    return temp;    
-    }
-    public ListNode add(ListNode l1, ListNode l2)
-    {
-        if(l1==null&&l2==null)
-            return null;
-        ListNode head= new ListNode(0);
-        ListNode pointer=head;
+        if(l1==null)
+            return l2;
+        if(l2==null)
+            return l1;
+        ListNode dummy = new ListNode(-1);
+        ListNode p= dummy;
         int carry=0;
-        while(l1!=null&&l2!=null)
-        {
-            int sum=l1.val+l2.val+carry;
-            carry=sum/10;
-            pointer.next=new ListNode(sum%10);
-            pointer=pointer.next;
-            l1=l1.next;
-            l2=l2.next;
-            
+        while(l1!=null||l2!=null){
+            int first = l1==null?0:l1.val;
+            int second = l2==null?0:l2.val;
+            int total = first+second+carry;
+            carry= total/10;
+            total = total%10;
+            ListNode newNode = new ListNode(total);
+            p.next=newNode;
+            l1= l1==null?null:l1.next;
+            l2= l2==null?null:l2.next;
+            p=p.next;
+        
         }
-        while(l1!=null)
-        {
-            int sum=l1.val+carry;
-            carry=sum/10;
-            pointer.next=new ListNode(sum%10);
-            pointer=pointer.next;
-            l1=l1.next;
-        }
-         while(l2!=null)
-        {
-            int sum=l2.val+carry;
-            carry=sum/10;
-            pointer.next=new ListNode(sum%10);
-            pointer=pointer.next;
-            l2=l2.next;
-        }
-        if(carry!=0)
-            pointer.next= new ListNode(carry);
-        return head.next;
+        
+        if(carry>0)
+            p.next= new ListNode(carry);
+        
+        return dummy.next;
+        
     }
-    
 }
