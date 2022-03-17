f279ba94e1ef01a181806706c882b8fc10431765
Manohar Golla <golamanohar.gm@gmail.com>
2022-03-17 05:43:37 +0530

    Time: 13 ms (58.24%), Space: 55.3 MB (33.49%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,40 +1,33 @@
 class Solution {
+
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, new Comparator<int[]>(){
-            public int compare(int[] a, int[] b){
-                return a[0]-b[0];
+        //         Arrays.sort(
+        //             intervals,
+        //             new Comparator<int[]>() {
+
+        //                 public int compare(int[] a, int[] b) {
+        //                     return a[0] - b[0];
+        //                 }
+        //             }
+        //         );
+
+        List<int[]> ans = new ArrayList<>();
+        if (intervals.length == 1) {
+            return intervals;
+        }
+        Arrays.sort(intervals, (a, b) -> a[0]- b[0]);
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        for (int[] i : intervals) {
+            if (i[0] <= end) {
+                end = Math.max(end, i[1]);
+            } else {
+                ans.add(new int[] { start, end });
+                start = i[0];
+                end = i[1];
             }
-        });
-        //System.out.println(Arrays.deepToString(intervals));
-        Stack<Pair> stack = new Stack<>();
-        stack.push(new Pair(intervals[0][0], intervals[0][1]));
-        for(int i=1;i<intervals.length;i++){
-            Pair p = stack.peek();
-            if(intervals[i][0]>=p.a && intervals[i][0]<=p.b){
-                stack.pop();
-                stack.push(new Pair(p.a, Math.max(p.b, intervals[i][1])));
-            }else{
-                stack.push(new Pair(intervals[i][0], intervals[i][1]));
-            }
         }
-       // System.out.println(stack);
-        int[][] ans = new int[stack.size()][2];
-        int i=0;
-        while(!stack.isEmpty()){
-            Pair p = stack.pop();
-            ans[i++]= new int[]{p.a, p.b};
-        }
-        return ans;
-    }
-    class Pair{
-        int a ;
-        int b;
-        Pair(int a, int b){
-            this.a=a;
-            this.b=b;
-        }
-        public String toString(){
-            return ""+this.a+" "+ this.b;
-        }
+        ans.add(new int[]{ start, end });
+        return ans.toArray(new int[ans.size()][2]);
     }
 }
