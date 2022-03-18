57895e73ffe8f0c2e33c0735c042bdad5fc04636
Manohar Golla <golamanohar.gm@gmail.com>
2022-03-18 18:10:10 +0530

    Time: 80 ms (60.89%), Space: 102.2 MB (37.55%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -1,18 +1,17 @@
 class Solution {
+
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (a,b)-> Integer.compare(a[1],b[1]));
-        int c=0;
-        
-       // System.out.println(Arrays.deepToString(intervals));
-        for(int i=1;i<intervals.length;i++){
-            if(intervals[i][0]<intervals[i-1][1]){
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+        int c = 0;
+
+        // System.out.println(Arrays.deepToString(intervals));
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < end) {
                 // cancelled the meeting, hence compare next with previous meeting
-                intervals[i][0]= intervals[i-1][0];
-                intervals[i][1]= intervals[i-1][1];
-                
+               // end = intervals[i - 1][1];
                 c++;
-            }
-                
+            } else end = intervals[i][1];
         }
         return c;
     }
