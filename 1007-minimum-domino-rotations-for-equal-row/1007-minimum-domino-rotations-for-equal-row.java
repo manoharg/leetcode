c632ec02ce2b7a931a45f7809c6a724308b682a1
Manohar Golla <golamanohar.gm@gmail.com>
2022-03-20 12:54:09 +0530

    Time: 6 ms (58.07%), Space: 49.3 MB (95.99%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -1,5 +1,19 @@
 class Solution {
+ /*
 
+  int[] countA = new int[7], countB = new int[7], same = new int[7];
+        int n = A.length;
+        for (int i = 0; i < n; ++i) {
+            countA[A[i]]++;
+            countB[B[i]]++;
+            if (A[i] == B[i])
+                same[A[i]]++;
+        }
+        for (int i  = 1; i < 7; ++i)
+            if (countA[i] + countB[i] - same[i] == n)
+                return n - Math.max(countA[i], countB[i]);
+        return -1;
+ */
     public int minDominoRotations(int[] tops, int[] bottoms) {
         int[] one = new int[7];
         for (int i = 0; i < tops.length; i++) one[tops[i]]++;
