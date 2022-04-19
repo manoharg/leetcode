076d26a36dd7a1f219847266e6ffc5c5a5d3ac7e
Manohar Golla <golamanohar.gm@gmail.com>
2022-04-19 23:36:06 +0530

    Time: 2 ms (87.87%), Space: 47.3 MB (48.33%) - LeetHub

---
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public void recoverTree(TreeNode root) {
+        TreeNode pre = null;
+        TreeNode first = null, second = null;
+        // Morris Traversal
+        TreeNode temp = null;
+		while(root!=null){
+			if(root.left!=null){
+				// connect threading for root
+				temp = root.left;
+				while(temp.right!=null && temp.right != root)
+					temp = temp.right;
+				// the threading already exists
+				if(temp.right!=null){
+				    if(pre!=null && pre.val > root.val){
+				        if(first==null){first = pre;second = root;}
+				        else{second = root;}
+				    }
+				    pre = root;
+				    
+					temp.right = null;
+					root = root.right;
+				}else{
+					// construct the threading
+					temp.right = root;
+					root = root.left;
+				}
+			}else{
+				if(pre!=null && pre.val > root.val){
+				    if(first==null){first = pre;second = root;}
+				    else{second = root;}
+				}
+				pre = root;
+				root = root.right;
+			}
+		}
+		// swap two node values;
+		if(first!= null && second != null){
+		    int t = first.val;
+		    first.val = second.val;
+		    second.val = t;
+		}
+    }
+}
