a0bebbafd7460ea4e4ddb5b85cbe9efb08a62f37
Manohar Golla <golamanohar.gm@gmail.com>
2022-05-29 07:28:10 +0530

    Time: 5 ms (45.13%), Space: 54.6 MB (49.93%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.java
+++ 986-interval-list-intersections/986-interval-list-intersections.java
@@ -1,22 +1,25 @@
 class Solution {
-
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-        List<int[]> res = new ArrayList<>();
-        if (firstList == null || secondList == null) return res.toArray(new int[0][]);
-        int i = 0;
-        int j = 0;
-
-        while (i < firstList.length && j < secondList.length) {
+        int fl = firstList.length;
+        int sl = secondList.length;
+        ArrayList<int[]> ret = new ArrayList<>();
+        if(firstList.length==0)
+            return new int[0][0];
+        if(sl==0)
+            return new int[0][0];
+        
+        int i=0,j=0;
+        while(i<fl&&j<sl){
             // overlapping part
             int start = Math.max(firstList[i][0], secondList[j][0]);
             int end = Math.min(firstList[i][1], secondList[j][1]);
             // valid overlap
             if (start <= end) {
-                res.add(new int[] { start, end });
+                ret.add(new int[] { start, end });
             }
-            // ignore the interval which is finishing early
+                  // ignore the interval which is finishing early
             if (firstList[i][1] == end) i++; else j++;
         }
-        return res.toArray(new int[0][]);
+        return ret.toArray(new int[1][2]);
     }
 }
