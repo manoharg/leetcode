30adabec611208087fa9c83c079b820779dae8a4
Manohar Golla <golamanohar.gm@gmail.com>
2022-05-31 06:39:58 +0530

    Time: 8 ms (62.45%), Space: 66.4 MB (51.48%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
@@ -1,29 +1,20 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        // Cycle sort
-        // when array contains 1...n , cycle sort can be used
-        int cur=0;
-        while(cur<nums.length){
-            int expectedIndex = nums[cur]-1;
-            if(nums[expectedIndex]!=nums[cur]){
-                swap(nums, cur, expectedIndex);
+        List<Integer> ret = new ArrayList<>();
+        int i=0;
+        while(i<nums.length){
+            int j= nums[i]-1;
+            if(nums[i]!=nums[j]){
+                int temp = nums[j];
+                nums[j]= nums[i];
+                nums[i]=temp;
             }else
-                cur++;
-            
+                i++;
         }
-        // System.out.println(Arrays.toString(nums));
-        List<Integer> ans = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=i+1)
-                ans.add(i+1);
+        for(i=0;i<nums.length;i++){
+            if(i+1!=nums[i])
+                ret.add(i+1);
         }
-        return ans;
-            
-    }
-    private void swap(int[] nums, int i, int j){
-        int temp = nums[j];
-        nums[j]=nums[i];
-        nums[i]=temp;
-        return;
+        return ret;
     }
 }
