734997e97f4e831e6d80d772121eed9a6d3027b5
Manohar Golla <golamanohar.gm@gmail.com>
2022-06-23 08:12:36 +0530

    Time: 1 ms (100.00%), Space: 57.5 MB (5.12%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count =1;
+        int maj = nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==maj)
+                count++;
+            else
+                count--;
+            if(count<0){
+                count=1;
+                maj=nums[i];
+            }
+        }
+        return maj;
+    }
+}
