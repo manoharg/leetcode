9dd1da533662ec0e4c4f0bcf5753f73b6fc8eb46
Manohar Golla <golamanohar.gm@gmail.com>
2022-06-25 07:33:08 +0530

    Time: 25 ms (89.14%), Space: 46.7 MB (87.68%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,42 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-  
-        if(nums==null || nums.length <3)
-          return ans;
         Arrays.sort(nums);
-        int sum=0;
-       // System.out.println(Arrays.toString(nums));
+        List<List<Integer>> ans = new ArrayList<>();
+           //     System.out.println(Arrays.toString(nums));
+
         for(int i=0;i<nums.length-2;i++){
-            // skip duplicates 
-            if(i>0 && nums[i]== nums[i-1])
+            if(i>0&&nums[i-1]==nums[i]){
                 continue;
-            int j = i+1;
-            search(nums, -nums[i], j,ans);
-            
+            }
+            search(nums, -nums[i], i+1, nums.length-1, ans);      
         }
         return ans;
     }
-    
-    private void search(int[] arr, int target, int start, List<List<Integer>> ans){
-        int end=arr.length-1;
-        while(start<end){
-            if(arr[start]+arr[end]==target){
-                ans.add(Arrays.asList(-target, arr[start], arr[end]));
-                start++;
-                end--;
-                
-                
-            while(start<end&&arr[end]==arr[end+1])
-                end--; //skip duplicates
-            while(start<end&&arr[start]==arr[start-1])
-                start++;    //skip duplicates
-                
-            }else if(arr[start]+arr[end]<target)
-                start++;
+    private void search(int[] nums, int tar, int l, int r,List<List<Integer>> ans){
+       // System.out.println(tar);
+        while(l<r){
+            if(nums[l]+nums[r]==tar){
+                ans.add(Arrays.asList(nums[l],  nums[r], -tar));
+                l++;
+                r--;
+            
+                while(l<r&&nums[l]==nums[l-1]){
+                    l++;
+                }
+                while(l<r&&nums[r]==nums[r]+1){
+                    r--;
+                }
+            }else if(nums[l]+nums[r]<tar)
+                l++;
             else
-                end--;
+                r--;
         }
         return;
+        
     }
 }
