54411c018bf81a9c9d182f334d081facb408e4a0
Manohar Golla <golamanohar.gm@gmail.com>
2022-07-12 20:25:17 +0530

    Time: 123 ms (68.01%), Space: 44.9 MB (89.06%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,5 +1,7 @@
 class Solution {
 
+    
+// TC : O(N* (M+ M^2))
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         final Set<String> wordSet = new HashSet<>(wordList);
         if (!wordSet.contains(endWord)) return 0;
@@ -11,19 +13,19 @@ class Solution {
         vis.add(beginWord);
 
         int count = 1;
-        while (!q.isEmpty()) {
+        while (!q.isEmpty()) { // O(N)
             final int size = q.size();
             for (int i = 0; i < size; i++) {
                 final String cur = q.poll();
 
-                if (cur.equals(endWord)) return count;
+                if (cur.equals(endWord)) return count; // O(M)
 
-                for (int j = 0; j < cur.length(); j++) {
+                for (int j = 0; j < cur.length(); j++) { // O(M)
                     for (int k = 'a'; k <= 'z'; k++) {
                         char[] arr = cur.toCharArray();
                         arr[j] = (char) k;
 
-                        final String str = new String(arr);
+                        final String str = new String(arr); // O(M)
 
                         if (wordSet.contains(str) && !vis.contains(str)) {
                             q.offer(str);
