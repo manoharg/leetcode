49edca8646dc7bd2b0b80a12f230c9ed922a1022
Manohar Golla <golamanohar.gm@gmail.com>
2022-07-31 12:14:45 +0530

    Time: 7 ms (44.77%), Space: 42.7 MB (69.77%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -0,0 +1,23 @@
+class Solution {
+  public String makeGood(String s) {
+        StringBuilder sb = new StringBuilder();
+        int n= s.length();
+        for(int i=0;i<s.length();){
+            if(i<n-1&&same(s.charAt(i),s.charAt(i+1))){
+                i+=2;
+            }else if(sb.length()>0 && same(s.charAt(i), sb.charAt(sb.length()-1))){
+                sb.deleteCharAt(sb.length()-1);
+                i++;
+            }else{
+                sb.append(s.charAt(i));
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+    private boolean same(Character one, Character two){
+       if(one==two)
+         return false;
+        return Character.toUpperCase(one) == two || Character.toUpperCase(two)==one;
+    }
+}
