71efb05ce44e96c8503b4665895e3efb92759995
Manohar Golla <golamanohar.gm@gmail.com>
2022-08-06 10:06:36 +0530

    Time: 66 ms (18.87%), Space: 58.2 MB (13.41%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,18 +1,24 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount+1];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        dp[0]=0;
-        for(int i=1;i<=amount;i++){
-            for(int j=0;j<coins.length;j++){
-                if(coins[j]<=i&& i-coins[j]>=0 && dp[i-coins[j]]!= Integer.MAX_VALUE){
-                    dp[i]= Math.min(dp[i], 1+ dp[i-coins[j]]);
-                }
+      int l = coins.length;
+       int[][] dp = new int[l+1][amount+1];
+      for(int i=0;i<l+1;i++){
+        for(int j=0;j<amount+1;j++){
+          if(i==0){
+            dp[i][j]= Integer.MAX_VALUE-1;
+            
+          }else if(j==0){
+            dp[i][j]=0;
+            
+          }else{
+            if(coins[i-1]<=j){
+              dp[i][j]= Math.min(dp[i-1][j], 1+dp[i][j-coins[i-1]]);
+            }else{
+              dp[i][j]=dp[i-1][j];
             }
+          }
         }
-        
-    
-        System.out.println(Arrays.toString(dp));
-        return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];
+      }
+      return dp[l][amount] == Integer.MAX_VALUE-1 ? -1: dp[l][amount];
     }
 }
