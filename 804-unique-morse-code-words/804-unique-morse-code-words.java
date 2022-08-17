7a30828b98b13b4fc784866945aef12f9d1bf723
Manohar Golla <golamanohar.gm@gmail.com>
2022-08-17 12:46:22 +0530

    Time: 4 ms (52.93%), Space: 41.8 MB (78.66%) - LeetHub

---
+++ 804-unique-morse-code-words/804-unique-morse-code-words.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] codes = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+      
+      Set<String> set = new HashSet<>();
+      for(String word: words){
+        StringBuilder sb = new StringBuilder();
+        for(char ch: word.toCharArray()){
+          sb.append(codes[ch-'a']);
+        }
+        set.add(sb.toString());
+      }
+      return set.size();
+    }
+}
